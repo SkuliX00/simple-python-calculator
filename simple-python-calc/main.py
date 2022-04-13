23eccd003cd15e826c0e3786ce78139b42cb8a9f
SkuliX00 <103485311+SkuliX00@users.noreply.github.com>
2022-04-13 15:39:20 +0200

    Add files via upload

---
+++ simple-python-calc/main.py
@@ -0,0 +1,16 @@
+class calculator:
+    def dodaj(x,y):
+        dodaj = x + y
+        print(dodaj)
+
+    def odejmij(x,y):
+        odejmij = x - y
+        print(odejmij)
+
+    def iloczyn(x,y):
+        razy = x * y
+        print(razy)
+
+    def iloraz(x,y):
+        iloraz = x / y
+        print(iloraz)
